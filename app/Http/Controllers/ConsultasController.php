e6bd9a3899a6c71118f6d76119ab48dec9e4d688
Maikol <maikolgonzalias7@gmail.com>
2026-05-25 13:43:18 -0500

    cambiar consultas a find(1) y rutas sin parámetros

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -9,50 +9,116 @@ use Illuminate\Http\Request;
 
 class ConsultasController extends Controller
 {
-    public function soldado($id)
-    {
-        $soldado = Soldier::find($id);
+
+
+// RUTAS QUE TENIA EN EL QUIZ ESTAN BIEN SOLO QUE YO LAS PUSE CON ID y NO DEJARLOS FIJOS CON find(1)
+
+    // public function soldado($id)
+    // {
+    //     $soldado = Soldier::find($id);
         
-        return response()->json([
-            'nombre' => $soldado->nombre,
-            'apellido' => $soldado->apellido,
-            'grado' => $soldado->grado
-        ]);
-    }
+    //     return response()->json([
+    //         'nombre' => $soldado->nombre,
+    //         'apellido' => $soldado->apellido,
+    //         'grado' => $soldado->grado
+    //     ]);
+    // }
 
-    public function soldados()
-    {
-        $soldados = Soldier::all();
-        return response()->json($soldados);
-    }
+    // public function soldados()
+    // {
+    //     $soldados = Soldier::all();
+    //     return response()->json($soldados);
+    // }
 
-    public function cuartel($id)
-    {
-        $cuartel = Quarter::find($id);
+    // public function cuartel($id)
+    // {
+    //     $cuartel = Quarter::find($id);
         
-        return response()->json([
-            'nombre' => $cuartel->nombre,
-            'ubicacion' => $cuartel->ubicacion
-        ]);
-    }
+    //     return response()->json([
+    //         'nombre' => $cuartel->nombre,
+    //         'ubicacion' => $cuartel->ubicacion
+    //     ]);
+    // }
 
 
-    public function cuarteles()
-    {
-        $cuarteles = Quarter::all();
-        return response()->json($Quarter);
+    // public function cuarteles()
+    // {
+    //     $cuarteles = Quarter::all();
+    //     return response()->json($Quarter);
 
-    }
-
-
-    public function servicio($id)
-    {
-        $servicio = Service::find($id);
+    // }
+    
+    // public function servicio($id)
+    // {
+    //     $servicio = Service::find($id);
         
-        return response()->json([
-            'actividad' => $servicio->actividad_servicio
-        ]);
+    //     return response()->json([
+    //         'actividad' => $servicio->actividad_servicio
+    //     ]);
+    // }
+
+
+
+
+//    RUTAS ACTUALIZADAS CON EL  find(1)
+
+
+    public function soldado()
+    {
+        $soldado = Soldier::find(1);
+
+        return [
+            'soldado' => $soldado,
+            'cuerpo' => $soldado->armyCorp,
+            'cuartel' => $soldado->quarter,
+            'compañia' => $soldado->company,
+            'servicios' => $soldado->services,
+        ];
     }
+
+    
+    public function cuartel()
+    {
+        $cuartel = Quarter::find(1);
+
+        return [
+            'cuartel' => $cuartel,
+            'soldados' => $cuartel->soldiers,
+        ];
+    }
+
+
+    public function servicio()
+    {
+        $servicio = Service::find(1);
+
+        return [
+            'servicio' => $servicio,
+            'soldados' => $servicio->soldiers,
+        ];
+    }
+
+    public function armyCorp()
+    {
+        $armyCorp = ArmyCorp::find(1);
+
+        return [
+            'cuerpo' => $armyCorp,
+            'soldados' => $armyCorp->soldiers,
+        ];
+    }
+
+    public function company()
+    {
+        $company = Company::find(1);
+
+        return [
+            'compañia' => $company,
+            'soldados' => $company->soldiers,
+        ];
+    }
+
+
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/soldado/{id}', [ConsultasController::class, 'soldado']);
-Route::get('/soldados', [ConsultasController::class, 'soldados']);
-Route::get('/servicio/{id}', [ConsultasController::class, 'servicio']);
-Route::get('/cuartel/{id}', [ConsultasController::class, 'cuartel']);
-Route::get('/cuarteles', [ConsultasController::class, 'cuarteles']);
+
+// RUTAS QUE TENIA EL QUIZ 
+
+// Route::get('/soldado/{id}', [ConsultasController::class, 'soldado']);
+// Route::get('/soldados', [ConsultasController::class, 'soldados']);
+// Route::get('/servicio/{id}', [ConsultasController::class, 'servicio']);
+// Route::get('/cuartel/{id}', [ConsultasController::class, 'cuartel']);
+// Route::get('/cuarteles', [ConsultasController::class, 'cuarteles']);
+
+
+// RUTAS ACTUALIZADAS sin {id}
+
+Route::get('/soldado', [ConsultasController::class, 'soldado']);
+Route::get('/cuartel', [ConsultasController::class, 'cuartel']);
+Route::get('/servicio', [ConsultasController::class, 'servicio']);
+Route::get('/cuerpo', [ConsultasController::class, 'armyCorp']);
+Route::get('/compañia', [ConsultasController::class, 'company']);
 
 
 
